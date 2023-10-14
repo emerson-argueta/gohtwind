9f2130a1f173eafde9ea672d3aba8f1ea1815f89
emerson.argueta <emerson.argueta@springscs.org>
2023-10-14 06:25:07 -0700

    added feature templates, refacted gen-feature script into gohtwind command

--- main.go
+++ main.go
@@ -17,10 +17,22 @@ func main() {
 	projectName := flag.String("name", "", "Name of the project to be generated")
 	flag.Parse()
 
-	if *projectName == "" {
-		fmt.Println("Please provide a project name using the -name flag.")
-		os.Exit(1)
+	if *projectName != "" {
+		generateProject(projectName)
+		return
 	}
+
+	featureName := flag.String("gen-feature", "", "Generate a new feature")
+	flag.Parse()
+
+	if *featureName != "" {
+		generateFeature(featureName)
+		return
+	}
+
+}
+
+func generateProject(projectName *string) {
 
 	// Directory structures
 	createDirs(*projectName, []string{
@@ -31,31 +43,62 @@ func main() {
 	})
 
 	// Copy and replace placeholders in templates
-	copyTemplate("main.go", "main.go", *projectName)
-	copyTemplate("package.json", "frontend/package.json", *projectName)
-	copyTemplate("postcss.config.js", "frontend/postcss.config.js", *projectName)
-	copyTemplate("main.css", "frontend/static/css/main.css", *projectName)
-	copyTemplate("tailwind.config.js", "frontend/tailwind.config.js", *projectName)
-	copyTemplate("dev-setup-linux.sh", "dev-setup-macos.sh", *projectName)
-	copyTemplate("dev-setup-macos.sh", "dev-setup-macos.sh", *projectName)
-	copyTemplate("dev-setup-windows.sh", "dev-setup-macos.sh", *projectName)
-	copyTemplate("Dockerfile.prod", "Dockerfile.prod", *projectName)
-	copyTemplate("gen-feature.sh", "gen-feature.sh", *projectName)
-	copyTemplate(".gitignore", ".gitignore", *projectName)
-	copyTemplate(".example.env", ".example.env", *projectName)
-	copyTemplate(".air.toml", ".air.toml", *projectName)
+	copyProjTemplate("main.go", "main.go", *projectName)
+	copyProjTemplate("package.json", "frontend/package.json", *projectName)
+	copyProjTemplate("postcss.config.js", "frontend/postcss.config.js", *projectName)
+	copyProjTemplate("main.css", "frontend/static/css/main.css", *projectName)
+	copyProjTemplate("tailwind.config.js", "frontend/tailwind.config.js", *projectName)
+	copyProjTemplate("dev-setup-linux.sh", "dev-setup-macos.sh", *projectName)
+	copyProjTemplate("dev-setup-macos.sh", "dev-setup-macos.sh", *projectName)
+	copyProjTemplate("dev-setup-windows.sh", "dev-setup-macos.sh", *projectName)
+	copyProjTemplate("Dockerfile.prod", "Dockerfile.prod", *projectName)
+	copyProjTemplate("gen-feature.sh", "gen-feature.sh", *projectName)
+	copyProjTemplate(".gitignore", ".gitignore", *projectName)
+	copyProjTemplate(".example.env", ".example.env", *projectName)
+	copyProjTemplate(".air.toml", ".air.toml", *projectName)
 
 	fmt.Printf("Project '%s' has been generated!\n", *projectName)
 }
 
+func generateFeature(featureName *string) {
+	// Directory structures
+	createDirs(*featureName, []string{
+		"static",
+		"static/js",
+		"static/css",
+		"templates",
+	})
+
+	// Copy and replace placeholders in templates
+	copyFeatureTemplate("handler.go.template", "handler.go", *featureName)
+	copyFeatureTemplate("routes.go.template", "routes.go", *featureName)
+	copyFeatureTemplate("view.go.template", "view.go", *featureName)
+	copyFeatureTemplate("create.html", "templates/create.html", *featureName)
+	copyFeatureTemplate("read.html", "templates/read.html", *featureName)
+	copyFeatureTemplate("update.html", "templates/update.html", *featureName)
+	copyFeatureTemplate("delete.html", "templates/delete.html", *featureName)
+	copyFeatureTemplate("list.html", "templates/list.html", *featureName)
+
+	fmt.Printf("Feature '%s' has been generated!\n", featureName)
+	fmt.Printf("Add the following to the main.go file:\n")
+	fmt.Printf("import \"%s\"\n", *featureName)
+	fmt.Printf("%s.SetupRoutes()\n", *featureName)
+
+}
 func createDirs(projectName string, dirs []string) {
 	for _, dir := range dirs {
 		os.MkdirAll(filepath.Join(projectName, dir), os.ModePerm)
 	}
 }
 
-func copyTemplate(src, dest, projectName string) {
+func copyProjTemplate(src, dest, projectName string) {
 	data, _ := templates.ReadFile("templates/" + src)
 	content := strings.ReplaceAll(string(data), "{{PROJECT_NAME}}", projectName)
 	os.WriteFile(filepath.Join(projectName, dest), []byte(content), os.ModePerm)
+}
+
+func copyFeatureTemplate(src, dest, featureName string) {
+	data, _ := templates.ReadFile("templates/feature_templates" + src)
+	content := strings.ReplaceAll(string(data), "{{FEATURE_NAME}}", featureName)
+	os.WriteFile(filepath.Join(featureName, dest), []byte(content), os.ModePerm)
 }
