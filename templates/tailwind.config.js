c0ab32737a3b7a6ae802f60fd5c06e2c813e548c
emerson.argueta <emerson.argueta@springscs.org>
2023-10-16 08:05:16 -0700

    updated frontend tooling

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -1,18 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: {
-        // Enable purging only in production for better dev experience
-        enabled: process.env.NODE_ENV === 'production',
-        content: [
-            './**/*.html',
-            './**/*.go',
-            // Add any other file types you might use
-            // where Tailwind classes are referenced
-        ],
-    },
-    darkMode: false, // or 'media' or 'class'
+    content: ["../**/*.{html,js,go,css}"],
     theme: {
         extend: {},
     },
-    variants: {},
     plugins: [],
 }
