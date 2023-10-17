e8b0c80888f909429bc21b0d0edc98235ae3dbab
emerson.argueta <emerson.argueta@springscs.org>
2023-10-17 11:20:30 -0700

    updated base and list templates, removed tailwind plugins, added util js function

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -4,9 +4,5 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/aspect-ratio'),
-    ],
+    plugins: [],
 }
--- templates/util.js
+++ templates/util.js
@@ -6,3 +6,16 @@ function toggleHidden(elementId) {
         elem.classList.add('hidden');
     }
 }
+
+function toggleClassesBetween(elementId, subsetClasses1Str, subsetClasses2Str) {
+    const subsetClasses1 = subsetClasses1Str.split(' ');
+    const subsetClasses2 = subsetClasses2Str.split(' ');
+    var elem = document.getElementById(elementId);
+    if (subsetClasses1.every(c => elem.classList.contains(c))) {
+        subsetClasses1.forEach(c => elem.classList.remove(c));
+        subsetClasses2.forEach(c => elem.classList.add(c));
+    } else {
+        subsetClasses2.forEach(c => elem.classList.remove(c));
+        subsetClasses1.forEach(c => elem.classList.add(c));
+    }
+}
