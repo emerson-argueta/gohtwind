325a2683480aac5087d11d202acf43d8504032ba
emerson.argueta <emerson.argueta@springscs.org>
2023-10-25 07:31:05 -0700

    updated repo partial

--- cmds/repo_template/repo_partial.go
+++ cmds/repo_template/repo_partial.go
@@ -1,6 +1,12 @@
 
 // CRUD operation
 
-func Create{{MODEL_NAME}}Repo(db *sql.DB) *{{MODEL_NAME}}Repo {
-	return &{{MODEL_NAME}}Repo{db: db}
+func All{{MODEL_NAME}}Repo(dbs map[string]*sql.DB) []struct{ model.{{MODEL_NAME}} } {
+	stmt := SELECT({{MODEL_NAME}}.AllColumns).FROM({{MODEL_NAME}}).LIMIT(10)
+	var dest []struct { model.{{MODEL_NAME}} }
+	err := stmt.Query(dbs["{{DB_NAME}}"], &dest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dest
 }
