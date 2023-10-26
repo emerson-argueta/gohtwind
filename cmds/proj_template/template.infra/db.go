f1ee04f64b5a6b931508393008b5ba4e3b44b1dc
emerson.argueta <emerson.argueta@springscs.org>
2023-10-26 06:22:29 -0700

    parsing database.yml file with env variable placeholders

--- cmds/proj_template/template.infra/db.go
+++ cmds/proj_template/template.infra/db.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
-	"gopkg.in/yaml.v3"
 	"log"
 	"os"
+	"strings"
 )
 
 type Database struct {
@@ -33,9 +33,15 @@ func NewDBsConfig() *DBsConfig {
 		log.Fatalf("error: %v", err)
 	}
 
+	// Replace placeholders with env variables
+	configSt, err := parseConfigTemplate(string(data))
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
 	//Unmarshal the file contents
 	var cfg DBsConfig
-	err = yaml.Unmarshal(data, &cfg)
+	err = yaml.Unmarshal([]byte(configSt), &cfg)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
@@ -43,46 +49,77 @@ func NewDBsConfig() *DBsConfig {
 	return &cfg
 }
 
-var (
-	dbSetupFuncMap = map[string]func(Database) (*sql.DB, error){
-		"mysql": setUpMysqlDB,
-		"psql":  setUpPsqlDB,
+func parseConfigTemplate(template string) (string, error) {
+	// Find placeholders in the format {{VAR_NAME}} and replace them with the actual environment variable values
+	placeholders := findPlaceholders(template)
+
+	for _, ph := range placeholders {
+		envValue, exists := os.LookupEnv(ph)
+		if !exists {
+			return "", fmt.Errorf("environment variable %s not set", ph)
+		}
+		template = strings.ReplaceAll(template, fmt.Sprintf("{{%s}}", ph), envValue)
 	}
-)
+
+	return template, nil
+}
+
+func findPlaceholders(template string) []string {
+	var placeholders []string
+	startIdx := 0
+	// algorithm: find the first occurrence of {{, then find the first occurrence of }} after that
+	// then extract the string between {{ and }} and add it to the placeholders slice
+	// then repeat the process until there are no more {{ }} pairs
+	for {
+		startIdx = strings.Index(template, "{{")
+		if startIdx == -1 {
+			break
+		}
+		endIdx := strings.Index(template, "}}")
+		if endIdx == -1 {
+			break
+		}
+		placeholders = append(placeholders, template[startIdx+2:endIdx])
+		template = template[endIdx+2:]
+	}
+	return placeholders
+}
+
+var dbSetupFuncMap = map[string]func(Database) (*sql.DB, error){
+	"mysql": func(d Database) (*sql.DB, error) {
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+			d.Username, d.Password, d.Host, d.Port, d.Database,
+		)
+		return setUpDB(d, "mysql", dsn)
+	},
+	"psql": func(d Database) (*sql.DB, error) {
+		dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			d.Host, d.Port, d.Username, d.Password, d.Database,
+		)
+		return setUpDB(d, "postgres", dsn)
+	},
+}
 
 func (cfg *DBsConfig) ConnectAll() (map[string]*sql.DB, error) {
 	dbs := make(map[string]*sql.DB)
-	for name, db := range cfg.Databases {
-		var d *sql.DB
+	for name, d := range cfg.Databases {
+		var db *sql.DB
 		var err error
-		if dbSetupFunc, ok := dbSetupFuncMap[db.Adapter]; ok {
-			d, err = dbSetupFunc(db)
-		} else {
-			return nil, fmt.Errorf("Adapter %s not supported", db.Adapter)
+		dbSetupFunc, ok := dbSetupFuncMap[d.Adapter]
+		if !ok {
+			return nil, fmt.Errorf("Adapter %s not supported", d.Adapter)
 		}
+		db, err = dbSetupFunc(d)
 		if err != nil {
 			return nil, err
 		}
-		dbs[name] = d
+		dbs[name] = db
 	}
 	return dbs, nil
 }
 
-func setUpMysqlDB(d Database) (*sql.DB, error) {
-	connectString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", d.Username, d.Password, d.Host, d.Port, d.Database)
-	db, err := sql.Open("mysql", connectString)
-	if d.MaxConn != 0 {
-		db.SetMaxOpenConns(d.MaxConn)
-	}
-	if d.MaxIdle != 0 {
-		db.SetMaxIdleConns(d.MaxIdle)
-	}
-	return db, err
-}
-
-func setUpPsqlDB(d Database) (*sql.DB, error) {
-	var connectString = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", d.Host, d.Port, d.Username, d.Password, d.Database)
-	db, err := sql.Open("postgres", connectString)
+func setUpDB(d Database, adapter string, dsn string) (*sql.DB, error) {
+	db, err := sql.Open(adapter, dsn)
 	if d.MaxConn != 0 {
 		db.SetMaxOpenConns(d.MaxConn)
 	}
