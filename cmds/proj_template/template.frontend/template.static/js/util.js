6034b3a5612b996fede5bbe165e8ece03d65563d
emerson.argueta <emerson.argueta@springscs.org>
2023-10-30 05:51:05 -0700

    fixed interactivity in sidebar

--- cmds/proj_template/template.frontend/template.static/js/util.js
+++ cmds/proj_template/template.frontend/template.static/js/util.js
@@ -1,21 +1,25 @@
-function toggleHidden(elementId) {
-    var elem = document.getElementById(elementId);
-    if (elem.classList.contains('hidden')) {
-        elem.classList.remove('hidden');
-    } else {
-        elem.classList.add('hidden');
-    }
+function toggleHidden(selector) {
+    var elems = document.querySelectorAll(selector);
+    elems.forEach(elem => {
+        if (elem.classList.contains('hidden')) {
+            elem.classList.remove('hidden');
+        } else {
+            elem.classList.add('hidden');
+        }
+    });
 }
 
-function toggleClassesBetween(elementId, subsetClasses1Str, subsetClasses2Str) {
+function toggleClassesBetween(selector, subsetClasses1Str, subsetClasses2Str) {
+    var elems = document.querySelectorAll(selector);
     const subsetClasses1 = subsetClasses1Str.split(' ');
     const subsetClasses2 = subsetClasses2Str.split(' ');
-    var elem = document.getElementById(elementId);
-    if (subsetClasses1.every(c => elem.classList.contains(c))) {
-        subsetClasses1.forEach(c => elem.classList.remove(c));
-        subsetClasses2.forEach(c => elem.classList.add(c));
-    } else {
-        subsetClasses2.forEach(c => elem.classList.remove(c));
-        subsetClasses1.forEach(c => elem.classList.add(c));
-    }
+    elems.forEach(elem => {
+        if (subsetClasses1.every(c => elem.classList.contains(c))) {
+            subsetClasses1.forEach(c => elem.classList.remove(c));
+            subsetClasses2.forEach(c => elem.classList.add(c));
+        } else {
+            subsetClasses2.forEach(c => elem.classList.remove(c));
+            subsetClasses1.forEach(c => elem.classList.add(c));
+        }
+    });
 }
