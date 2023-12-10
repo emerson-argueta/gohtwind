5c06eeebe9eaacf181359d3ae64a643e26c38689
emerson.argueta <emerson.argueta@springscs.org>
2023-12-10 07:07:23 -0800

    refactored entrypoint

--- cmds/proj_template/main.go
+++ cmds/proj_template/main.go
@@ -8,54 +8,80 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"{{PROJECT_NAME}}/auth"
 	"{{PROJECT_NAME}}/infra"
 )
 
 func main() {
-	envFileName := setUpEnv()
+	setUpEnv()
+	dbs := setUpDBs()
+	setUpRoutes(dbs)
+	setUpServer(dbs)
+}
+
+func setUpEnv() {
+	env := flag.String("env", "development", "Environment (production or development)")
+	flag.Parse()
+	var envFileName string
+	if *env == "production" {
+		return
+	}
+	envFileName = ".env"
 	err := godotenv.Load(envFileName)
 	if err != nil {
 		log.Fatal("Error loading env file")
 	}
-	port := os.Getenv("PORT")
+}
 
+func setUpDBs() map[string]*sql.DB {
 	dbs, err := infra.NewDBsConfig().ConnectAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, db := range dbs {
-		defer func(db *sql.DB) {
-			err := db.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}(db)
 		infra.CheckDatabaseConnection(db)
 	}
-	http.Handle("/static/", infra.LoggingMiddleware(http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/static/")))))
-	// TODO: Setup routes and middleware
-
-	auth.SetupRoutes(dbs, infra.LoggingMiddleware)
-	// Then activate the routes
-	infra.ActivateRoutes()
-
-	log.Printf("Server started on :%s\n", port)
-	err = http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return dbs
 }
 
-func setUpEnv() string {
-	// Define the -env flag
-	env := flag.String("env", "development", "Environment (production or development)")
-	flag.Parse()
-	var envFileName string
-	if *env == "production" {
-		envFileName = ".env.production"
-	} else {
-		envFileName = ".env"
+func setUpRoutes(dbs map[string]*sql.DB) {
+	http.Handle("/static/", infra.LoggingMiddleware(http.StripPrefix("/static/", http.FileServer(http.Dir("./frontend/static/")))))
+	auth.SetupRoutes(dbs, infra.LoggingMiddleware)
+	// TODO: Add more routes here
+
+	// Then activate the routes
+	infra.ActivateRoutes()
+}
+
+func setUpServer(dbs map[string]*sql.DB) {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		log.Printf("Received signal: %s", sig)
+		done <- true
+	}()
+
+	go func() {
+		port := os.Getenv("PORT")
+		log.Printf("Server started on :%s\n", port)
+		if err := http.ListenAndServe(":"+port, nil); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	<-done
+	log.Println("Shutting down server...")
+
+	for name, db := range dbs {
+		if err := db.Close(); err != nil {
+			log.Printf("Error closing database: %v", err)
+		}
+		log.Printf("Database %s closed", name)
 	}
-	return envFileName
+
+	log.Println("Server gracefully stopped")
 }
