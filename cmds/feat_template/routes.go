51f3dcad67dac9ab5d88d1cac17ddb817b1a70ae
emerson.argueta <emerson.argueta@springscs.org>
2023-12-24 09:37:17 -0800

    fixed static route for features

--- cmds/feat_template/routes.go
+++ cmds/feat_template/routes.go
@@ -10,15 +10,14 @@ var feature_router *infra.Router
 
 func SetupRoutes(dbs map[string]*sql.DB, middleware ...infra.Middleware) {
 	handle := &Handle{dbs: dbs}
-	shan := http.StripPrefix("/static/{{FEATURE_NAME}}/", http.FileServer(http.Dir("./{{FEATURE_NAME}}/static/")))
 	routes := []infra.Route{
 		{Method: "GET", Handler: handle.List, Path: "/{{FEATURE_NAME}}"},
 		{Method: "POST", Handler: handle.Create, Path: "/{{FEATURE_NAME}}"},
 		{Method: "GET", Handler: handle.Read, Path: "/{{FEATURE_NAME}}/{id}"},
 		{Method: "PATCH", Handler: handle.Update, Path: "/{{FEATURE_NAME}}/{id}"},
 		{Method: "DELETE", Handler: handle.Delete, Path: "/{{FEATURE_NAME}}/{id}"},
-		{Method: "GET", Handler: shan.ServeHTTP, Path: "/static/{{FEATURE_NAME}}/"},
 	}
+	http.Handle("/{{FEATURE_NAME}}/static/",http.StripPrefix("/{{FEATURE_NAME}}/static/", http.FileServer(http.Dir("./{{FEATURE_NAME}}/static/"))))
 	feature_router = infra.NewRouter(routes)
 	feature_router.SetupRoutes(middleware...)
 }
